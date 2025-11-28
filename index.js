c85771f111e9c4db094b35b0febad1694e7cca7a
riyasalvi-git <riyasalvi@protonmail.com>
2025-11-28 18:52:51 +0530

    create one row

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const container = document.querySelector("#container");
 const square = document.createElement("div");
+let copy;
+square.setAttribute("style", "border: 1px solid #414447; background-color: white; height: 30px; width: 30px;");
+container.append(square);
+
+//create one row (since container is flex)
+for (let i = 0; i < 15; i++) {
+	copy = square.cloneNode();
+	container.insertBefore(copy, square);
+}
+
